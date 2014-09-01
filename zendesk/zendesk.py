73071fdccbb71c70675c472d19930f2245a918d5
Ilya Shargorodskiy <ilyash18@gmail.com>
2014-09-01 21:34:07 +0400

    Update zendesk.py
    Fix 01.09. Inject the credentials in all requests.

--- zendesk/zendesk.py
+++ zendesk/zendesk.py
@@ -189,12 +189,9 @@ class Zendesk(object):
             # the 'search' endpoint in an open Zendesk site doesn't return a
             # 401 to force authentication. Inject the credentials in the
             # headers to ensure we get the results we're looking for
-            if re.match("^/search\..*", path):
                 self.headers["Authorization"] = "Basic %s" % (
                     base64.b64encode(self.zendesk_username + ':' +
                                      self.zendesk_password))
-            elif "Authorization" in self.headers:
-                del(self.headers["Authorization"])
 
             # Make an http request (data replacements are finalized)
             response, content = \
